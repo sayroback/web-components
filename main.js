60ec6e5467dbcdc9e9d7728d184eb17ee08f1dd2
Juan Manuel Calderon Robles <sayroback@gmail.com>
2022-09-01 21:27:53 -0500

    implementando el shadow dom

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 import { myElement } from "./components/my-element.js";
 import { myElementTemplate } from "./components/my-element-template.js";
+import { shadowDom } from "./components/shadow-dom.js";
